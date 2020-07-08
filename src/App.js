8733ccb49c73817612464ac6cfe2395b5384766e
bluemaxg <ihavecontrol@ucomp.co.kr>
2020-07-08 18:39:56 +0900

    Update App.js
    test

--- src/App.js
+++ src/App.js
@@ -11,15 +11,15 @@ function Food({name, picture}){
 }
 
 const foodILike = [{
-  name : "F-15",
+  name : "F15",
   image : "https://i.redd.it/5mtrag24lavz.jpg"
 },
 {
-  name : "F-18",
+  name : "F18",
   image : "https://upload.wikimedia.org/wikipedia/commons/thumb/3/30/US_Navy_050702-N-5781F-165_An_F-A-18E_Super_Hornet_assigned_to_Strike_Fighter_Squadron_Two_Seven_(VFA-27)_performs_during_an_air_power_demonstration.jpg/1920px-US_Navy_050702-N-5781F-165_An_F-A-18E_Super_Hornet_assigned_to_Strike_Fighter_Squadron_Two_Seven_(VFA-27)_performs_during_an_air_power_demonstration.jpg"
 },
 {
-  name : "F-16",
+  name : "F16",
   image : "https://nationalinterest.org/sites/default/files/main_images/f-16_fighting_falcon.jpg"
 }];
 
