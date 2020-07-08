1ff67d2882a4666357846473666fe108abdb32df
bluemaxg <ihavecontrol@ucomp.co.kr>
2020-07-08 18:36:01 +0900

    day1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+function Food({name, picture}){ 
+  
+  return (
+    <div> 
+      <h2>i like {name}</h2>
+      <img src={picture} />
+    </div>
+  )
+}
+
+const foodILike = [{
+  name : "F-15",
+  image : "https://i.redd.it/5mtrag24lavz.jpg"
+},
+{
+  name : "F-18",
+  image : "https://upload.wikimedia.org/wikipedia/commons/thumb/3/30/US_Navy_050702-N-5781F-165_An_F-A-18E_Super_Hornet_assigned_to_Strike_Fighter_Squadron_Two_Seven_(VFA-27)_performs_during_an_air_power_demonstration.jpg/1920px-US_Navy_050702-N-5781F-165_An_F-A-18E_Super_Hornet_assigned_to_Strike_Fighter_Squadron_Two_Seven_(VFA-27)_performs_during_an_air_power_demonstration.jpg"
+},
+{
+  name : "F-16",
+  image : "https://nationalinterest.org/sites/default/files/main_images/f-16_fighting_falcon.jpg"
+}];
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+      {foodILike.map(dish => <Food name={dish.name} picture={dish.image} />)}
     </div>
   );
 }
