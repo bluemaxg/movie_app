173642bf8f0815007c1b4492769f1b0e49b27c02
bluemaxg <ihavecontrol@ucomp.co.kr>
2020-07-09 16:14:07 +0900

    props-types. rating 업데이트

--- src/App.js
+++ src/App.js
@@ -1,32 +1,56 @@
-import React from 'react';
+import React from "react";
+import PropTypes from "prop-types";
 
-function Food({name, picture}){ 
-  
+function Food({ name, picture, rating }) {
   return (
-    <div> 
+    <div>
       <h2>i like {name}</h2>
-      <img src={picture} />
+      <h4>{rating}/5</h4>
+      <img src={picture} alt={name} />
     </div>
-  )
+  );
 }
 
-const foodILike = [{
-  name : "F15",
-  image : "https://i.redd.it/5mtrag24lavz.jpg"
-},
-{
-  name : "F18",
-  image : "https://upload.wikimedia.org/wikipedia/commons/thumb/3/30/US_Navy_050702-N-5781F-165_An_F-A-18E_Super_Hornet_assigned_to_Strike_Fighter_Squadron_Two_Seven_(VFA-27)_performs_during_an_air_power_demonstration.jpg/1920px-US_Navy_050702-N-5781F-165_An_F-A-18E_Super_Hornet_assigned_to_Strike_Fighter_Squadron_Two_Seven_(VFA-27)_performs_during_an_air_power_demonstration.jpg"
-},
-{
-  name : "F16",
-  image : "https://nationalinterest.org/sites/default/files/main_images/f-16_fighting_falcon.jpg"
-}];
+Food.propTypes = {
+  name: PropTypes.string.isRequired,
+  picture: PropTypes.string.isRequired,
+  rating: PropTypes.number.isRequired,
+};
+
+const foodILike = [
+  {
+    id: 1,
+    name: "F15",
+    image: "https://i.redd.it/5mtrag24lavz.jpg",
+    rating: 5,
+  },
+  {
+    id: 2,
+    name: "F18",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/3/30/US_Navy_050702-N-5781F-165_An_F-A-18E_Super_Hornet_assigned_to_Strike_Fighter_Squadron_Two_Seven_(VFA-27)_performs_during_an_air_power_demonstration.jpg/1920px-US_Navy_050702-N-5781F-165_An_F-A-18E_Super_Hornet_assigned_to_Strike_Fighter_Squadron_Two_Seven_(VFA-27)_performs_during_an_air_power_demonstration.jpg",
+    rating: 4,
+  },
+  {
+    id: 3,
+    name: "F16",
+    image:
+      "https://nationalinterest.org/sites/default/files/main_images/f-16_fighting_falcon.jpg",
+    rating: 3,
+  },
+];
 
 function App() {
   return (
-    <div >
-      {foodILike.map(dish => <Food name={dish.name} picture={dish.image} />)}
+    <div>
+      {foodILike.map((dish) => (
+        <Food
+          key={dish.id}
+          name={dish.name}
+          picture={dish.image}
+          rating={dish.rating}
+        />
+      ))}
     </div>
   );
 }
