f1e4098f9c7c7f0629bc62f02da51afbba5274c3
bluemaxg <ihavecontrol@ucomp.co.kr>
2020-07-09 18:31:26 +0900

    commit

--- src/App.js
+++ src/App.js
@@ -1,58 +1,33 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-function Food({ name, picture, rating }) {
-  return (
-    <div>
-      <h2>i like {name}</h2>
-      <h4>{rating}/5</h4>
-      <img src={picture} alt={name} />
-    </div>
-  );
-}
+class App extends React.Component {
+    state = {
+        count: 0,
+    };
 
-Food.propTypes = {
-  name: PropTypes.string.isRequired,
-  picture: PropTypes.string.isRequired,
-  rating: PropTypes.number.isRequired,
-};
+    add = () => {
+        this.setState(current => ({ count: current.count + 1 }));
+    };
 
-const foodILike = [
-  {
-    id: 1,
-    name: "F15",
-    image: "https://i.redd.it/5mtrag24lavz.jpg",
-    rating: 5,
-  },
-  {
-    id: 2,
-    name: "F18",
-    image:
-      "https://upload.wikimedia.org/wikipedia/commons/thumb/3/30/US_Navy_050702-N-5781F-165_An_F-A-18E_Super_Hornet_assigned_to_Strike_Fighter_Squadron_Two_Seven_(VFA-27)_performs_during_an_air_power_demonstration.jpg/1920px-US_Navy_050702-N-5781F-165_An_F-A-18E_Super_Hornet_assigned_to_Strike_Fighter_Squadron_Two_Seven_(VFA-27)_performs_during_an_air_power_demonstration.jpg",
-    rating: 4,
-  },
-  {
-    id: 3,
-    name: "F16",
-    image:
-      "https://nationalinterest.org/sites/default/files/main_images/f-16_fighting_falcon.jpg",
-    rating: 3,
-  },
-];
+    minus = () => {
+        this.setState(current => ({ count: current.count - 1 }));
+    };
 
-function App() {
-  return (
-    <div>
-      {foodILike.map((dish) => (
-        <Food
-          key={dish.id}
-          name={dish.name}
-          picture={dish.image}
-          rating={dish.rating}
-        />
-      ))}
-    </div>
-  );
+    mul = () => {
+        this.setState(current => ({ count: current.count * 2 }));
+    };
+
+    render() {
+        return (
+            <div>
+                <h1>The number is: {this.state.count}</h1>
+                <button onClick={this.add}>Add</button>
+                <button onClick={this.minus}>Minus</button>
+                <button onClick={this.mul}>Mul</button>
+            </div>
+        );
+    }
 }
 
 export default App;
