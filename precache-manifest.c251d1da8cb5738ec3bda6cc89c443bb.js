2af500127f80d1297c612374e1b8e1f8040a2c4d
bluemaxg <ihavecontrol@ucomp.co.kr>
2020-07-13 13:32:12 +0900

    Updates

---
+++ precache-manifest.c251d1da8cb5738ec3bda6cc89c443bb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "420f95a0654400100fc97f126df09737",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "546ae7ca53694f529ae3",
+    "url": "/movie_app/static/css/main.640a586e.chunk.css"
+  },
+  {
+    "revision": "4cfc5f63e20591eee8d9",
+    "url": "/movie_app/static/js/2.ebd5af47.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/movie_app/static/js/2.ebd5af47.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "546ae7ca53694f529ae3",
+    "url": "/movie_app/static/js/main.141cc998.chunk.js"
+  },
+  {
+    "revision": "8a294428b8499a7b0c17",
+    "url": "/movie_app/static/js/runtime-main.9ac0a6e2.js"
+  }
+]);
